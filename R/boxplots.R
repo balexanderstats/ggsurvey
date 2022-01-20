0d4834c92843688afe1307c089b722139bd44b7d
Brittany Alexander <Brittany.Alexander@ipsos.com>
2022-01-20 18:38:18 -0500

    examples for boxplots

--- R/boxplots.R
+++ R/boxplots.R
@@ -8,22 +8,30 @@
 #' @export
 #'
 #' @examples
+#' library(survey)
+#' data(api)
+#' ggboxweight(apistrat, api00, pw)
+#' data(election)
+#' ggboxweight(election_pps, Bush, p)
 ggboxweight = function(df, x, weights){
   return(ggplot(df, aes({{x}}))+geom_boxplot(aes(weight = {{weights}})))}
 
 #' Weighted Box Plot with a categorical variable
 #'
 #' @param df data frame
-#' @param x categorical variable of interest
-#' @param y numeric variable of interest
+#' @param x numeric variable of interest
+#' @param y categorical variable of interest
 #' @param weights survey weights that sums to sample size
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' library(survey)
+#' data(api)
+#' ggboxweight2d(apistrat, api00, stype, pw)
 ggboxweight2d = function(df, x, y, weights){
-  return(ggplot(df, aes({{x}}, {{y}}))+geom_boxplot(aes(x = {{x}}, y = {{y}}, weight = {{weights}})))}
+  return(ggplot(df, aes({{y}}, {{x}}))+geom_boxplot(aes(x = {{y}}, y = {{x}}, weight = {{weights}})))}
 
 #' Weighted Box Plot with a categorical x axis and a faceting variable
 #'
@@ -37,5 +45,9 @@ ggboxweight2d = function(df, x, y, weights){
 #' @export
 #'
 #' @examples
+#' library(survey)
+#' data(api)
+#' ggboxweight3d(apistrat, api00, stype,awards, pw)
+#' ggboxweight3d(apistrat, api00, stype, awards, pw, binwidth = 10)
 ggboxweight3d = function(df, x, y, z, weights){
-  return(ggplot(df, aes({{x}}, {{y}}))+geom_boxplot(aes(x = {{x}}, y = {{y}}, weight = {{weights}}))+facet_grid(rows =vars({{z}})))}
+  return(ggplot(df, aes({{y}}, {{x}}))+geom_boxplot(aes(x = {{y}}, y = {{x}}, weight = {{weights}}))+facet_grid(rows =vars({{z}})))}
